44e90b8432b1464ac1a225b3de008e1e947c9937
AbdullahMuhammed5 <abdo56668@gamil.com>
2020-04-15 21:14:56 +0200

    refactor

--- resources/views/includes/SideSection.blade.php
+++ resources/views/includes/SideSection.blade.php
@@ -3,7 +3,7 @@
         <div class="art1 mb-4">
             <img src="{{Storage::url($news['cover']['path'])}}" alt="" class="w-100 img-fluid mb-3">
             <a href="{{ route('front.article', $news['id']) }}"><h5>{{$news['main_title']}}</h5></a>
-            <p class='block-with-text'>{!!substr($news['secondary_title'], 0, 200)."..."!!}</p>
+            <p class='block-with-text'>{!! substr($news['secondary_title'], 0, 200)."..." !!}</p>
         </div>
     @endforeach
 @endisset
